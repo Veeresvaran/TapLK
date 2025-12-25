9bfc688f213452848dc87fc4860edb80464a8d98
Veeresvaran <vveeresvaran@gmail.com>
2025-12-25 12:40:17 +0530

    Add categories model, template, and images

--- TapLK/settings.py
+++ TapLK/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
-MEDIA_ROOT = BASE_DIR / 'statics'
+MEDIA_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
