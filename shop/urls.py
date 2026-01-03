4d7eba9c7a96d5a797c3432d04d17307f602a3e3
Veeresvaran <vveeresvaran@gmail.com>
2026-01-03 14:18:16 +0530

    add can able to view product descrition

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register', views.register, name='register'),
-    path('collection', views.collection, name='collection'),
+    path('collection/', views.collection, name='collection'),
+    path('collection/<str:name>/', views.collectionview, name='collectionview'),
+    path('collection/<str:cname>/<str:pname>', views.product_details, name='product_detail'),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
+from django.contrib import messages
+
 
 
 def home(request) :
@@ -8,8 +10,28 @@ def home(request) :
 def register(request) :
     return render(request, 'shop/register.html')
 
+
 def collection(request) :
     category = Category.objects.filter(status=0)
     return render(request, 'shop/collection.html', {'category':category})
 
+def collectionview(request,name) :
+    if (Category.objects.filter(name=name, status=0)):
+        products = Product.objects.filter(Category__name=name)
+        return render(request, 'shop/products/index.html',{'products':products, 'category_name':name})
+    else:
+        messages.warning(request, "No such category found")
+        return redirect('collection')
 
+def product_details(request, cname, pname) :
+    if (Category.objects.filter(name=cname, status=0)):
+        if (Product.objects.filter(name=pname, status=0)):
+            products = Product.objects.filter(name=pname, status=0).first()
+            return render(request, 'shop/products/product_details.html',{'products':products})
+        else:
+            messages.error(request, "No such product found")
+            return redirect('collection')
+    else:
+        messages.error(request,"No such category found")
+        return redirect('collection')
+    
